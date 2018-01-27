33fd69e30f95ccf94c2541f59517f48702fdf8f6
Maria Cristoforo <mariasoccer2123@gmail.com>
2018-01-27 15:25:10 -0500

    Updated commands pneumatics

---
+++ src/org/usfirst/frc2084/CMonster2018/commands/gearboxShift.java
@@ -0,0 +1,47 @@
+package org.usfirst.frc2084.CMonster2018.commands;
+
+import org.usfirst.frc2084.CMonster2018.Robot;
+import org.usfirst.frc2084.CMonster2018.RobotMap;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class gearboxShift extends Command{
+	
+	private static DoubleSolenoid shiftingGearboxSolenoid = RobotMap.shiftingGearboxSolenoid;
+	
+	public gearboxShift() {
+		
+	}
+	
+	 protected void initialize() {
+	    	//move the pneumatic to shift gears in the gearbox
+		 	Robot.driveBase.shifted = !Robot.driveBase.shifted;
+		 
+	    }
+
+	 
+	    // Called repeatedly when this Command is scheduled to run
+	    @Override
+	    protected void execute() {
+	    }
+	    
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
+	 // Called once after isFinished returns true
+    @Override
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+    }
+	
+}
